9b668bf188264c7f80c8158b7fd234a501859753
Your Name <wrnermontala@gmail.com>
2024-12-10 16:46:01 +0800

    added clock function

---
+++ index.js
@@ -0,0 +1,17 @@
+//clock function
+
+const clockElement = document.getElementById("clock")
+
+function updateTime() {
+  const now = new Date()
+  const hours = now.getHours()
+  const minutes = now.getMinutes()
+  const seconds = now.getSeconds()
+
+  const clockStr = `${hours.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`
+
+  clockElement.innerHTML = clockStr
+}
+
+updateTime();
+setInterval(updateTime, 1000)
